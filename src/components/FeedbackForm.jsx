afe5f2fea3ef4b6a100ccc09383e3198f8a64291
realsulty <Sultanobm1@gmail.com>
2024-01-04 23:25:02 +0000

    new handlers

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,10 +4,24 @@ import Button from "./shared/Button"
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if (text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if ( text !== null && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text Must be At Least 10 Characters')
+        } else {
+            setBtnDisabled(false)
+            setMessage(null)
+        }
+
         setText(e.target.value)
     }
+
 
   return (
 <Card>
@@ -16,11 +30,12 @@ function FeedbackForm() {
         <div className="input-group">
             <input type="text"
             onChange={handleTextChange}
-            placeholder="Wriffftse a review"
+            placeholder="Wrtie a review"
             value={text}
             />
-            <Button type="submit">Send</Button>
+            <Button type="submit" isDisabled={btnDisabled} >Send</Button>
         </div>
+    {message && <div className="message">{message}</div>}
     </form>
 </Card>
   )
